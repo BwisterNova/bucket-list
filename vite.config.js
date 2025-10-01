9b66f2e51b96a27b6f608ee7a51110b0710b5c27
BwisterNova <bnovalife@gmail.com>
2025-10-01 07:18:09 +0100

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bucket-list",
+  base: process.env.DEPLOY_TARGET === "GH" ? "/bucket-list" : "/",
 });
